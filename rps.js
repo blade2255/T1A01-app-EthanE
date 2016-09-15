9492567836af24c28ef1bf53628d4c566c0a8bfc
Ethan E <blade@Ethans-MacBook-Pro.local>
2016-09-15 00:52:10 -0700

    Fixed and added for loop

--- rps.js
+++ rps.js
@@ -1,37 +1,46 @@
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-    computerChoice = "rock";
-} else if (computerChoice <= 0.67) {
-    computerChoice = "paper";
-} else {
-    computerChoice = "scissors";
-}
-console.log("Computer: " + computerChoice);
+var rockPaperScissors = function() {
+    var userChoice = prompt("Do you choose rock, paper or scissors?");
+    var computerChoice = Math.random();
+    var myOutput = document.getElementById("output");
+    var lucky = 1;
+    if (computerChoice < 0.34) {
+        computerChoice = "rock";
+    } else if (computerChoice <= 0.67) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+    console.log("Computer: " + computerChoice);
 
-var compare = function(choice1, choice2) {
-    if (choice1 === choice2) {
-        return "The result is a tie!";
-    } else if (choice1 === "rock") {
-        if (choice2 === "scissors") {
-            return "rock wins";
-        } else {
-            return "paper wins";
+    var compare = function(choice1, choice2) {
+        if (choice1 === choice2) {
+            myOutput.innerHTML = "The result is a tie!";
+        }
+        if (choice1 === "rock") {
+            if (choice2 === "scissors") {
+                myOutput.innerHTML = "The Computer chose Scissors, you win with Rock. <br>";
+            } else {
+                myOutput.innerHTML = "The Computer chose Paper, you lose with Rock. <br>";
+            }
+        }
+        if (choice1 === "paper") {
+            if (choice2 === "rock") {
+                myOutput.innerHTML = "The Computer chose Rock, you win with Paper. <br>";
+            } else {
+                myOutput.innerHTML = "The Computer chose scissors, you lose with Paper. <br>";
+            }
+        }
+        if (choice1 === "scissors") {
+            if (choice2 === "rock") {
+                myOutput.innerHTML = "The Computer chose Rock, you lose with Scissors. <br>";
+            } else {
+                myOutput.innerHTML = "The Computer chose Paper, you win with Scissors. <br>";
+            }
         }
     }
-    if (choice1 === "paper") {
-        if (choice2 === "rock") {
-            return "paper wins";
-        } else {
-            return "scissors wins";
-        }
-    }
-    if (choice1 === "scissors") {
-        if (choice2 === "rock") {
-            return "rock wins";
-        } else {
-            return "scissors wins";
-        }
-    }
+    compare(userChoice, computerChoice);
+ for(i=0;i<5;i++){
+    lucky *= Math.random()*100;
+  }
+  myOutput.innerHTML += "Your lucky number is: <br> " + lucky;
 }
-compare(userChoice, computerChoice)
