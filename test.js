d94bf79ee347b2822a38b4bb97a8897c69c75b45
Ethan E <blade@Ethans-MacBook-Pro.local>
2016-09-29 12:18:48 -0700

    fixed naming

---
+++ test.js
@@ -0,0 +1,20 @@
+//myString = 'Ethan'
+//myInt = 62
+//myReal = 62.3578
+//myBoolean = True
+//myChar = 'E'
+//if (myString[3]=='a'){
+//}
+
+myIntArray = new Array()
+myIntArray[0] = 57
+myIntArray[2] = 56
+myIntArray[3] = 45
+myIntArray[4] = 34
+myIntArray[5] = 27
+myIntArray[6] = 13
+myIntArray[7] = 98
+myIntArray[8] = 73
+myIntArray[9] = 64
+
+document.writeIn(myIntArray[7])
